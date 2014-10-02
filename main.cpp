9f13e24de14c520fd0deabfedbb60d00dc54bc51
Raymond Haeb <ray.haeb@googlemail.com>
2014-10-02 14:52:15 +0200

    now uses unsigned char

--- main.cpp
+++ main.cpp
@@ -18,19 +18,19 @@ int main( int argc, char *argv[] )
         return 1;
     std::istreambuf_iterator<char> beg(in_file);
     std::istreambuf_iterator<char> end;
-    std::vector<char> content;
+    std::vector<unsigned char> content;
     std::copy( beg, end, std::back_inserter( content ) );
     std::ofstream out_file;
     out_file.open( argv[2] );
     if( !out_file.is_open() )
         return 1;
-    out_file << "char const " << argv[3] << "[" << content.size() << "] =\n{";
+    out_file << "unsigned char const " << argv[3] << "[" << content.size() << "] =\n{";
     for( int i=0; i<content.size(); ++i )
     {
         if( i )
             out_file << ",";
         out_file << ( ( i%16 ) ? " " : "\n\t" );
-        out_file << "0x" << std::hex << std::setw( 2 ) << std::setfill( '0' )<< static_cast<int>( content[i] );
+        out_file << "0x" << std::hex << std::setw( 2 ) << std::setfill( '0' )<< static_cast<unsigned int>( content[i] );
     }
     out_file << "\n};\n";
     return 0;
