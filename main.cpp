6667856a06ff32b9a3e8853a40bb67fe41325977
Raymond Haeb <ray.haeb@googlemail.com>
2014-10-04 16:16:04 +0200

    #pragma once line-endings with std::endl

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 
 int main( int argc, char *argv[] ) {
     if (argc != 4) {
-        std::cout << "usage:\n"
+        std::cout << "usage:" << std::endl
                 << argv[0] << " inputfilename outputfilename arrayname\n";
         return 1;
     }
@@ -15,7 +15,7 @@ int main( int argc, char *argv[] ) {
         std::ifstream in_file;
         in_file.open(argv[1]);
         if (!in_file.is_open()) {
-            std::cerr << "cannot open input file: \"" << argv[1] << "\"\n";
+            std::cerr << "cannot open input file: \"" << argv[1] << "\"" << std::endl;
             return 1;
         }
         std::copy(
@@ -31,27 +31,29 @@ int main( int argc, char *argv[] ) {
     out_file_cpp.open( out_file_name_cpp );
     if( !out_file_cpp.is_open() )
     {
-        std::cerr << "cannot open output file: \"" << out_file_name_cpp << "\"\n";
+        std::cerr << "cannot open output file: \"" << out_file_name_cpp << "\"" << std::endl;
         return 1;
     }
     std::ofstream out_file_hpp;
     out_file_hpp.open( out_file_name_hpp );
     if( !out_file_hpp.is_open() )
     {
-        std::cerr << "cannot open output file: \"" << out_file_name_hpp << "\"\n";
+        std::cerr << "cannot open output file: \"" << out_file_name_hpp << "\"" << std::endl;
         return 1;
     }
-    out_file_hpp << "#include <array>\n\n";
-    out_file_cpp << "#include <array>\n\n";
-    out_file_hpp << "extern std::array<char const," << content.size() << "> " << argv[3] << ";\n";
-    out_file_cpp << "std::array<char const," << content.size() << "> " << argv[3] << " =\n{";
+    out_file_hpp << "#pragma once" << std::endl;
+    out_file_hpp << "#include <array>" << std::endl << std::endl;
+    out_file_cpp << "#include <array>" << std::endl << std::endl;
+    out_file_hpp << "extern std::array<char const," << content.size() << "> " << argv[3] << ";" << std::endl;
+    out_file_cpp << "std::array<char const," << content.size() << "> " << argv[3] << " ="  << std::endl << "{";
     for( int i=0; i<content.size(); ++i )
     {
         if( i )
             out_file_cpp << ",";
-        out_file_cpp << ( ( i%16 ) ? " " : "\n\t" );
-        out_file_cpp << std::setw( 4 ) << std::setfill( ' ' ) << static_cast<int>( content[i] );
+        if( !( i%16 ) )
+            out_file_cpp << std::endl << "\t";
+        out_file_cpp << std::setw( 5 ) << std::setfill( ' ' ) << static_cast<int>( content[i] );
     }
-    out_file_cpp << "\n};\n";
+    out_file_cpp << std::endl << "};" << std::endl;
     return 0;
 }
