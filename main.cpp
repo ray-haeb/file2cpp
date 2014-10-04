e92f79198590f7449468ed9c2ab3add0106b7eb4
Ray <ray.haeb@googlemail.com>
2014-10-04 12:45:19 +0200

    header-file created

--- main.cpp
+++ main.cpp
@@ -3,35 +3,54 @@
 #include <fstream>
 #include <vector>
 #include <iomanip>
+#include <string>
 
-int main( int argc, char *argv[] )
-{
-    if( argc != 4 )
-    {
+int main( int argc, char *argv[] ) {
+    if (argc != 4) {
         std::cout << "usage:\n"
-            << argv[0] << " inputfilename outputfilename arrayname\n";
+                << argv[0] << " inputfilename outputfilename arrayname\n";
         return 1;
     }
-    std::ifstream in_file;
-    in_file.open(argv[1]);
-    if( !in_file.is_open() )
-        return 1;
-    std::istreambuf_iterator<char> beg(in_file);
-    std::istreambuf_iterator<char> end;
     std::vector<unsigned char> content;
-    std::copy( beg, end, std::back_inserter( content ) );
-    std::ofstream out_file;
-    out_file.open( argv[2] );
-    if( !out_file.is_open() )
+    {
+        std::ifstream in_file;
+        in_file.open(argv[1]);
+        if (!in_file.is_open()) {
+            std::cerr << "cannot open input file: \"" << argv[1] << "\"\n";
+            return 1;
+        }
+        std::copy(
+                std::istreambuf_iterator<char>(in_file),
+                std::istreambuf_iterator<char>(),
+                std::back_inserter(content)
+        );
+        content.push_back(0);
+    }
+    std::string out_file_name_cpp = std::string( argv[2] ).append( ".cpp" );
+    std::string out_file_name_hpp = std::string( argv[2] ).append( ".hpp" );
+    std::ofstream out_file_cpp;
+    out_file_cpp.open( out_file_name_cpp );
+    if( !out_file_cpp.is_open() )
+    {
+        std::cerr << "cannot open output file: \"" << out_file_name_cpp << "\"\n";
         return 1;
-    out_file << "unsigned char const " << argv[3] << "[" << content.size() << "] =\n{";
+    }
+    std::ofstream out_file_hpp;
+    out_file_hpp.open( out_file_name_hpp );
+    if( !out_file_hpp.is_open() )
+    {
+        std::cerr << "cannot open output file: \"" << out_file_name_hpp << "\"\n";
+        return 1;
+    }
+    out_file_hpp << "extern char " << argv[3] << "[" << content.size() << "];\n";
+    out_file_cpp << "char " << argv[3] << "[" << content.size() << "] =\n{";
     for( int i=0; i<content.size(); ++i )
     {
         if( i )
-            out_file << ",";
-        out_file << ( ( i%16 ) ? " " : "\n\t" );
-        out_file << "0x" << std::hex << std::setw( 2 ) << std::setfill( '0' )<< static_cast<unsigned int>( content[i] );
+            out_file_cpp << ",";
+        out_file_cpp << ( ( i%16 ) ? " " : "\n\t" );
+        out_file_cpp << std::setw( 3 ) << std::setfill( ' ' ) << static_cast<unsigned int>( content[i] );
     }
-    out_file << "\n};\n";
+    out_file_cpp << "\n};\n";
     return 0;
 }
